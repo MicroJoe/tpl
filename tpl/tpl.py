d9610cae9d3fd464e0720eead371ad924cb58870
Romain Porte <microjoe@mailoo.org>
2014-12-13 00:38:35 +0100

    Added license and docstrings in source files

--- tpl/tpl.py
+++ tpl/tpl.py
@@ -1,9 +1,22 @@
 # coding: utf-8
+
+# This file is part of tpl.
+# Copyright (C) 2014 Romain 'MicroJoe' PORTE.
 #
-# TPL
-# A simple template-based file creator.
+# tpl is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-# Copyright (C) Romain PORTE (MicroJoe)
+# tpl is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with tpl. If not, see <http://www.gnu.org/licenses/>.
+
+"""Main module of the program."""
 
 import sys
 import os
@@ -68,3 +81,4 @@ def run(*args, **kwargs):
     context['project'] = os.path.basename(os.getcwd())
 
     print(render_template(sys.argv[1], sys.argv[2]))
+
